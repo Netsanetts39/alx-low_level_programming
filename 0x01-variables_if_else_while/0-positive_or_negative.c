d6a69fc689b693e1c4fa723f90458d5a47c2b6a4
Netsanetts39 <106772761+Netsanetts39@users.noreply.github.com>
2022-07-01 04:36:09 -0700

    Create 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+intmain (void)
+{
+  int n;
+  
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  /* your code goes there */
+  return (0) ;
+}
