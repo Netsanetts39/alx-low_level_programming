c4ef593a06495712e78171645f2c131b306654cf
Netsanetts39 <106772761+Netsanetts39@users.noreply.github.com>
2022-07-01 06:52:54 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,13 +13,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 printf("Last digit of %d is ",n);
 if (n > 5)
-{
-printf("greater than 5");
-}
+ {
+    printf("greater than 5");
+ }
 if (n == 0)
-{
-printf("0");
-}
+ {
+    printf("0");
+ }
 if (n < 6 && n !=0);
 {
 printf("Less than 6 not 0");
