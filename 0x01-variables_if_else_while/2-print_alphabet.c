5f2f999afe99a70007ba374d2cec21c560ef85d0
Netsanetts39 <106772761+Netsanetts39@users.noreply.github.com>
2022-07-03 10:17:42 -0700

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
-void main()
+int main()
 {
-
-        int ch;
-clrscr();
-
-        printf(“Print all uppercase letters followed by all lowercase letters on the next line :\n”);
-
-        /* print uppercase letters */
-
-        for (ch= ‘A’; ch<= ‘Z’; ch++)
-
-             putchar(ch);
-
-             putchar (‘\n’ ) ;
-
-             /* print lowercase letters */
-
-             for (ch= ‘a’; ch<= ‘z’; ch++)
-
-                  putchar(ch);
-
-                  putchar (‘\n’) ;
-  getch();
-
+        char ch = 'a';
+        
+        while(ch <= 'z')
+        {
+                printf("%c " ch);
+                ch++;
+        }
+        
+        return 0;
 }
+
+        
