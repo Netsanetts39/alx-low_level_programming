dd74342461a6a7b1ef7b2a2e3f38468c34984a4d
Netsanetts39 <106772761+Netsanetts39@users.noreply.github.com>
2022-07-03 12:08:03 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,7 +14,6 @@
 int main(void)
 {
 	int digit1, digit2, digit3;
-	
 	for (digit1 = 0; digit1 < 8; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
@@ -25,7 +24,7 @@ int main(void)
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-		continue;
+					continue;
 				putchar(',');
 				putchar(' ');
 			}
