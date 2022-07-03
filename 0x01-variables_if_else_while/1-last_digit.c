0b9ab70a0d6df2ae970b02171cea59b3e389b908
Netsanetts39 <106772761+Netsanetts39@users.noreply.github.com>
2022-07-03 11:11:22 +0300

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-#include <stdio.h>
-/**
+/*
 *assign a random number to the variable n each time it is executed
 *Return : always 0
 */
-int main(void)
+int main(void);
 {
 int n;
 srand(time(0));
