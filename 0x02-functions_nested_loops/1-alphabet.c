87e432a8faf22476b2cfbc10064f6ab7b8f9653d
Netsanetts39 <106772761+Netsanetts39@users.noreply.github.com>
2022-07-05 10:56:51 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,18 +2,14 @@
  * File: 1-alphabet.c
  * Auth: Netsanet
  */
-
 #include "main.h"
-
 /**
- * main - prints the alphabet in lowercase, followed by a new line.
- */
+* main - prints the alphabet in lowercase, followed by a new line.
+*/
 void print_alphabet(void);
 {
 	char letter;
-
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
-
 	_putchar('\n');
 }
